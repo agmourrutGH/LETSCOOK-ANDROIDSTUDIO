2fe1c46530d6596d5fd4c2c3ca873c003327cf41
Agustin <agusmourrut@gmail.com>
2024-10-30 21:30:30 -0300

    Correccion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
